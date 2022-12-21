5b4a2a185fc8d2b5f4de5d41a1247b97682c05b8
wilfex <wilfex81@gmail.com>
2022-12-21 20:48:47 +0300

    files

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+angle.append(rowlist)
+    return triangle"""
+Defines function that returns a list of lists of integers
+representing the Pascal's triangle of n
+"""
+
+
+def pascal_triangle(n):
+    """
+    Creates a list of lists of integers representing Pascal's triangle
+
+    parameters:
+        n [int]:
+            the number of rows of Pascal's triangle to recreate
+
+    return:
+        [list of lists of ints]:
+            representation of Pascal's triangle
+    """
+    if type(n) is not int:
+        raise TypeError("n must be an integer")
+    triangle = []
+    if n <= 0:
+        return triangle
+    previous = [1]
+    for row_index in range(n):
+        rowlist = []
+        if row_index == 0:
+            rowlist = [1]
+        else:
+            for i in range(row_index + 1):
+                if i == 0:
+                    rowlist.append(0 + previous[i])
+                elif i == (row_index):
+                    rowlist.append(previous - 1 + 0        else:[i])
